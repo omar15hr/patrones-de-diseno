761055d21108cc590c21ffadbf473418e50d9d45
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-02 09:25:24 -0300

    Tarea con el patron builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Patrón Builder:
  * Es un patrón de diseño creacional que nos permite construir objetos complejos
@@ -12,3 +13,78 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+
+class Computer {
+  public cpu: string = 'cpu - not defined';
+  public ram: string = 'ram - not defined';
+  public storage: string = 'storage - not defined';
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`
+      CPU: ${this.cpu}
+      RAM: ${this.ram}
+      Storage: ${this.storage}
+      GPU: ${this.gpu || 'no tiene GPU'}
+    `);
+  }
+}
+
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCpu(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRam(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+
+  setGpu(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build(): Computer {
+    return this.computer;
+  }
+}
+
+
+function main() {
+  const basicComputer = new ComputerBuilder()
+  .setCpu('Intel Core i3')
+  .setRam('8GB')
+  .setStorage('256GB')
+  .build();
+
+  console.log('Basic Computer:', COLORS.blue);
+  basicComputer.displayConfiguration();
+
+  const gamingComputer = new ComputerBuilder()
+  .setCpu('Intel Core i7')
+  .setRam('16GB')
+  .setStorage('2TB')
+  .setGpu('NVIDIA GeForce RTX 3060')
+  .build();
+
+  console.log('Gaming Computer:', COLORS.green);
+  gamingComputer.displayConfiguration();
+  
+}
+
+main();
